e8bca4f942231d87f9ee55a2a9d8d31154417665
Kirill Vorozhishchev <1507kirill1998@mail.ru>
2021-08-10 19:44:00 +0700

    Добавьте файлы проекта.

---
+++ HomeWork_19.5.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Animal
+{
+private:
+    int x;
+public:
+    virtual void Voice()
+    {
+        std::cout << "Animal Class" << '\n';
+    }
+};
+
+
+class Dog : public Animal
+{
+private:
+    double y;
+public:
+    void Voice() override
+    {
+        std::cout << "Woof!" << '\n';
+    }
+};
+
+
+class Cat : public Animal
+{
+private:
+    double z;
+public:
+    void Voice() override
+    {
+        std::cout << "Meow!" << "\n";
+    }
+};
+
+
+class Hamster : public Animal
+{
+private:
+    double w;
+public:
+    void Voice() override
+    {
+        std::cout << "beeps!";
+    }
+};
+
+
+int main()
+{
+    Animal* p1 = new Dog;
+    Animal* p2 = new Cat;
+    Animal* p3 = new Hamster;
+
+    Animal* nArr[] = { p1,p2,p3 };
+    for (int i = 0; i < 3; i++)
+    {
+        nArr[i]->Voice();
+    }
+    
+
+    return 0;
+}
